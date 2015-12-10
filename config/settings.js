832829ff3e17fac1044699fc046b65fb7b54e87e
lehighkid <lehighkid@gmail.com>
2015-12-10 15:46:01 -0500

    Significant cleanup and refactoring

--- config/settings.js
+++ config/settings.js
@@ -4,32 +4,13 @@
 // expose our settings directly to our application using module.exports
 module.exports = {
   milight: {
-    wifiboxip: '10.0.1.',
+    wifiboxip: '10.0.1.7',
     wifiboxport: '8899'
   },
-  razberry: {
-    zboxip: '10.0.1.',
-    zboxport: '8083',
-    zpath: 'ZWaveAPI/Run/devices[0].instances[0].commandClasses[98].Set({0})'
-  },
-  nest: {
-    execcmd: 'nest --conf /home/pi/commands/nest.conf temp'
-  },
-  rfcodesend: {
-    execcmd: 'sudo /home/pi/commands/rfoutlet/codesend {0}'
-  },
-  lightshowpi: {
-    execcmd: 'sudo python /home/pi/lightshowpi/py/hardware_controller.py --light {0} --state {1}'
-  },
-  gdoor: {
-    execpin: '16',
-    closedpin: '22',
-    openedpin: '25',
-    deviceid: ''
-
-  },
-  dasher: {
-    cottonelle: "",
-    dashes: ""
+  mqtt: {
+    rftopic: 'codesend/{0}/{1}',
+    lsptopic: 'lightshowpi/{0}/{1}',
+    gpiotopic: 'gpio/{0}/{1}',
+    broker: 'mqtt://10.0.1.4:1883'
   }
 };
